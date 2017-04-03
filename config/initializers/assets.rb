27002b72a9c886270b2c0730e0921ed17a190619
bdcajicam <bdcajicam@unal.edu.co>
2017-04-02 19:35:23 -0500

    Agregado layout de la unal.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,11 +2,25 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
-Rails.application.config.assets.precompile += %w( home.css )
-Rails.application.config.assets.precompile += %w( about.css )
-Rails.application.config.assets.precompile += %w( w3.css )
-Rails.application.config.assets.precompile += %w( font-awesome.css )
-Rails.application.config.assets.precompile += %w( font-lato.css )
+Rails.application.config.assets.precompile += %w( old/home.css )
+Rails.application.config.assets.precompile += %w( old/about.css )
+Rails.application.config.assets.precompile += %w( old/w3.css )
+Rails.application.config.assets.precompile += %w( old/font-awesome.css )
+Rails.application.config.assets.precompile += %w( old/font-lato.css )
+
+Rails.application.config.assets.precompile += %w( css/bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( css/bootstrap-theme.min.css )
+Rails.application.config.assets.precompile += %w( css/reset.css )
+Rails.application.config.assets.precompile += %w( css/unal.css )
+#Rails.application.config.assets.precompile += %w( css/base.css )
+#Rails.application.config.assets.precompile += %w( css/tablet.css )
+#Rails.application.config.assets.precompile += %w( css/phone.css )
+#Rails.application.config.assets.precompile += %w( css/small.css )
+#Rails.application.config.assets.precompile += %w( css/printer.css )
+
+Rails.application.config.assets.precompile += %w( js/jquery.js )
+Rails.application.config.assets.precompile += %w( js/unal.js )
+
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
