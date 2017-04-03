527868bd6a594d9f2ac672eeb2efc49ebdecc614
bdcajicam <bdcajicam@unal.edu.co>
2017-04-02 19:35:23 -0500

    AddFile en contributions y carrierwave
    primera fase de lectur de archivos

--- Gemfile
+++ Gemfile
@@ -27,12 +27,11 @@ gem 'devise'
 gem 'omniauth'
 gem 'rack-cors', :require => 'rack/cors'
 
+gem 'carrierwave', '~> 0.10.0'
 
 #gem 'wdm', '>= 0.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
-# Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
@@ -59,7 +58,7 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 #WINDOWS Gems That don't play well with nix
-platforms :mswin do 
+platforms :mswin do
   gem "wdm", :group => [:development, :test]
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170331122223) do
+ActiveRecord::Schema.define(version: 20170407032035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20170331122223) do
     t.integer  "investigation_group_id"
     t.datetime "created_at",                                     null: false
     t.datetime "updated_at",                                     null: false
+    t.string   "file"
     t.index ["investigation_group_id"], name: "index_contributions_on_investigation_group_id", using: :btree
   end
 
